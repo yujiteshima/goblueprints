9965f261f731c746ebe0fe9ec4b96e19fcf68f09
Yuji Teshima <yuji.teshima@gmail.com>
2022-06-22 11:18:26 +0900

    modified main.go

---
+++ chat/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+				<head>
+					<title>チャット</title>
+				</head>
+				<body>
+					チャットしましょう！
+				</body>
+			</html>
+			`))
+	})
+	// start web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
